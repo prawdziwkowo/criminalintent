d0cb345bfdb3b7d4682d762ef601009041a8eede
Grzegorz Prawdzik <g.prawdzik@gmail.com>
2018-12-11 17:54:35 +0100

    rozdział 10 - ładne przekazywanie danych pomiędzy fragmetem a aktywnościa

--- app/src/main/java/pl/it_developers/criminalintent/CrimeActivity.java
+++ app/src/main/java/pl/it_developers/criminalintent/CrimeActivity.java
@@ -7,7 +7,7 @@ import android.support.v4.app.Fragment;
 import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
-    public static final String EXTRA_CRIME_ID =
+    private static final String EXTRA_CRIME_ID =
             "pl-it_developers.android.criminalintent.crime_id";
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
@@ -18,6 +18,7 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
